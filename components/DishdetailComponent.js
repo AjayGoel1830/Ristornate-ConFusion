b48a3c063d7bc78d9518ad4a614515bc1d420add
AjayGoel1830 <ajay1830goel@gmail.com>
2020-04-26 01:16:10 +0530

    Gesture Right to left to add Favorite with rubberBand Animation

--- components/DishdetailComponent.js
+++ components/DishdetailComponent.js
@@ -22,6 +22,7 @@ const mapDispatchToProps = dispatch => ({
 function RenderDish(props) {
 
     const dish = props.dish;
+    handleViewRef = ref => this.view = ref;
 
     const recognizeDrag = ({ moveX, moveY, dx, dy }) => {
         if ( dx < -200 )
@@ -33,6 +34,10 @@ function RenderDish(props) {
     const panResponder = PanResponder.create({
         onStartShouldSetPanResponder: (e, gestureState) => {
             return true;
+        },
+        onPanResponderGrant: () => {
+            this.view.rubberBand(1000)
+            .then(endState => console.log(endState.finished ? 'finished' : 'cancelled'));
         },
         onPanResponderEnd: (e, gestureState) => {
             console.log("pan responder end", gestureState);
@@ -50,10 +55,11 @@ function RenderDish(props) {
             return true;
         }
     })
-    
+
         if (dish != null) {
             return(
                 <Animatable.View animation="fadeInDown" duration={2000} delay={1000}
+                ref={this.handleViewRef}
                 {...panResponder.panHandlers}>
                     <Card
                     featuredTitle={dish.name}
